b6779ac64bcaa37bd4738e3dcdcb0eb5d2d658e0
Bonfry <info@bonfry.com>
2024-09-07 12:09:37 +0200

    removed old codebase

--- lib/transpiler.dart
+++
@@ -1,7 +0,0 @@
-transpileKotlinToDart(){
-
-}
-
-transpileDartToKotlin(){
-
-}
--- test/compose_transpiler_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:compose_transpiler/compose_transpiler.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
